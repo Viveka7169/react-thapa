32ceb01f89098164285e2351993dc5c3a4a2ccb5
Viveka7169 <viveka7169@gmail.com>
2025-01-26 09:04:25 +0530

    Practice file created and Added

---
+++ src/Components/Practice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Pracite=()=>{
+    const students=[1,2,3,4,5,6]
+return(
+    <>
+    <p>{students.length===0 && "No Students Found"}</p>
+    <p>No of Students:{students.length}</p>
+    </>
+)
+}
+export default Pracite
