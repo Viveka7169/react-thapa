a3d17de3ca2ea4f9c86d0d8ccd60289449fff6d4
Viveka7169 <viveka7169@gmail.com>
2025-01-28 14:30:22 +0530

    Add Netflic Card Component in MetflixSeris.jsx

--- src/Components/NetflixCard.jsx
+++ src/Components/NetflixCard.jsx
@@ -1,24 +1,52 @@
 import React from 'react'
 
-const NetflixCard = (props) => {
-  return (
-    <>
-     <li>
-                        <img src={props.curElement.img_url} alt={props.curElement.name} width="40%" height="40%" />
+// const NetflixCard = (props) => {
+//   return (
+//     <>
+//      <li>
+//                         <img src={props.curElement.img_url} alt={props.curElement.name} width="20%" height="20%" />
                         
-                        <p>Name:{props.curElement.name}</p>
-                        <p>Rating:{props.curElement.rating}</p>
-                        <p>Summery:{props.curElement.description}</p>
-                        <p>Genere:{props.curElement.genre}</p>
-                        <p>Cast:{props.curElement.cast}</p>
-                        <a href={props.curElement.watch_url}>
-                            <button>Watch Now</button>
-                        </a>
+//                         <p>Name:{props.curElement.name}</p>
+//                         <p>Rating:{props.curElement.rating}</p>
+//                         <p>Summery:{props.curElement.description}</p>
+//                         <p>Genere:{props.curElement.genre}</p>
+//                         <p>Cast:{props.curElement.cast}</p>
+//                         <a href={props.curElement.watch_url}>
+//                             <button>Watch Now</button>
+//                         </a>
              
 
-                    </li>
-    </>
-  )
-}
+//                     </li>
+//     </>
+//   )
+// }
 
-export default NetflixCard
+// export default NetflixCard
+
+
+
+// *****************************Another Way****************************************
+const NetflixCard = ({data}) => {
+    console.log(data);
+    const {id,name,img_url,rating,description,genre,cast,watch_url} =data
+    return (
+      <>
+       <li>
+                          <img src={img_url} alt={name} width="40%" height="20%" />
+                          
+                          <p>Name:{name}</p>
+                          <p>Rating:{rating}</p>
+                          <p>Summery:{description}</p>
+                          <p>Genere:{genre}</p>
+                          <p>Cast:{cast}</p>
+                          <a href={watch_url}>
+                              <button>Watch Now</button>
+                          </a>
+               
+  
+                      </li>
+      </>
+    )
+  }
+  
+  export default NetflixCard
--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import seriesData from "../api/seriesData.json"
 import NetflixCard from "./NetflixCard";
 const NetflixSeries=()=>{
-console.log(seriesData);
+// console.log(seriesData);
     return(
         <ul>
         <h1>Netflix Series</h1>
         {
             seriesData.map((currData)=>{
                 return(
-                   <NetflixCard key={currData.id} curElement={currData}/>
+                   <NetflixCard key={currData.id} data={currData}/>
                 )
             })
         }
