0c8ed0a5d62f207001143b87768b871dd7e4a11c
Viveka7169 <viveka7169@gmail.com>
2025-02-04 12:22:06 +0530

    Trying to fetch data with async await

--- src/Projects/Pokemon.jsx
+++ src/Projects/Pokemon.jsx
@@ -5,19 +5,23 @@ const Pokemon = () => {
     const [loading,setLoading]=useState(true)
     const [error,setError]=useState(null)
     const API='https://pokeapi.co/api/v2/pokemon/pikachu'
+    const fetchApi=async()=>{
+    try{
+        const res=await fetch(API)
+        const data=await res.json()
+        setPokemon(data)
+        setLoading(false)
+    }catch(error){
+        console.log(error);
+        setError(error)
+        setLoading(false)
+
+
+    }
+
+    }
     useEffect(()=>{
-        fetch(API)
-        .then((res)=>res.json())
-        .then((data)=>{
-            setPokemon(data)
-            setLoading(false)
-        })
-       
-        .catch((err)=>{
-            setError(err)
-            setLoading(false)
-        }
-        )
+        fetchApi()
 
     },[])
     console.log(pokemon);
